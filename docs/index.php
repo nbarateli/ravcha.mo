2c82f00055db60aa94cb1ead65f9695ad3707635
Niko Barateli <nbara15@freeuni.edu.ge>
2018-07-03 01:32:17 +0400

    added index.php for pre-rendering html

---
+++ docs/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style/reset.css">
+  <link rel="stylesheet" href="style/main.css">
+  <title>ravcha.mo - თქვენი საკვები პორტალი!</title>
+</head>
+<body>
+<div id="header">head tavi</div>
+<div id="main">
+  <form id="ingredients">
+    <div class="ingredient">
+      <input>
+      <button>+</button>
+    </div>
+  </form>
+</div>
+<div id="footer">foot feet</div>
+</body>
+</html>
