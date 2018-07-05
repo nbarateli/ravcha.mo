0be208f20d0dfe0e0bd9a4dd38cb3e84db1793ce
nbarateli <nbara15@freeuni.edu.ge>
2018-07-05 12:52:30 +0400

    determined default fonts and colors

--- docs/index.php
+++ docs/index.php
@@ -6,6 +6,7 @@
 <html lang="en">
 <head>
   <link rel="stylesheet" type="text/css" href="docs/less/main.less"/>
+  <link rel="stylesheet" type="text/css" href="docs/less/main.less"/>
   <meta charset="UTF-8">
   <link rel="stylesheet" href="style/reset.css">
   <link rel="stylesheet" href="style/main.css">
@@ -14,28 +15,30 @@
 <body>
 <div id="header">head tavi</div>
 <div id="main">
-  <form id="ingredients">
-    <div id="ingredient-list">
-        <?php for ($i = 0; $i < 5; $i++) { ?>
-          <div class="ingredient">
-            <label>
-              <small>დასახელება</small>
-              <input name="ingredient_name" autocomplete="off">
-            </label>
-            <label>რაოდენობა
-              <input name="ingredient_quantity" type="number">
-            </label>
-            <select name="quantity_type">
-              <option>გრამი</option>
-              <option>კგ.</option>
-              <option>ცალი</option>
-            </select>
-          </div>
-        <?php } ?>
-    </div>
-    <button id="add_field">+</button>
-    <input type="submit" value="ძებნა">
-  </form>
+  <div id="container">
+    <form id="ingredients">
+      <div id="ingredient-list">
+          <?php for ($i = 0; $i < 5; $i++) { ?>
+            <div class="ingredient">
+              <label>
+                <small>დასახელება</small>
+                <input name="ingredient_name" autocomplete="off">
+              </label>
+              <label>რაოდენობა
+                <input name="ingredient_quantity" type="number">
+              </label>
+              <select name="quantity_type">
+                <option>გრამი</option>
+                <option>კგ.</option>
+                <option>ცალი</option>
+              </select>
+            </div>
+          <?php } ?>
+      </div>
+      <button id="add_field">+</button>
+      <input type="submit" value="ძებნა">
+    </form>
+  </div>
 </div>
 <div id="footer">foot feet</div>
 <script src="script/main.js"></script>
